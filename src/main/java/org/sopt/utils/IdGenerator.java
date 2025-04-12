780d4f81185059e557dbfb209ca06c9999f7f315
Minkyu Shin <digi1k2001@gmail.com>
2025-04-12 16:46:43 +0900

    refactor: IdGenerator

--- src/main/java/org/sopt/utils/IdGenerator.java
+++ src/main/java/org/sopt/utils/IdGenerator.java
@@ -3,7 +3,7 @@ package org.sopt.utils;
 public class IdGenerator {
     private static int postId = 1;
 
-    public int generatePostId() {
+    public static int generatePostId() {
         return postId++;
     }
 }
