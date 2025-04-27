a963010b16804ff401f9c411ea527d0ad543036a
Minkyu Shin <digi1k2001@gmail.com>
2025-04-28 00:59:58 +0900

    fix: 텍스트 길이 계산 로직(ZWJ, 수정자 체크)

--- src/main/java/org/sopt/domain/post/util/TextLengthUtil.java
+++ src/main/java/org/sopt/domain/post/util/TextLengthUtil.java
@@ -4,17 +4,39 @@ import java.text.BreakIterator;
 import java.util.Locale;
 
 public class TextLengthUtil {
+
+    private static final int ZWJ = 0x200D;
+
     private TextLengthUtil() {}
 
     public static int visibleLength(String text) {
-        BreakIterator breakIterator = BreakIterator.getCharacterInstance(Locale.ROOT);
-        breakIterator.setText(text);
-
         int length = 0;
-        while (breakIterator.next() != BreakIterator.DONE) {
+        for (int i = 0; i < text.length(); ) {
+            int codePoint = text.codePointAt(i);
+            i += Character.charCount(codePoint);
+
+            // ex: 🧑‍🧑‍🧒‍🧒: 이모지 + ZWJ + 이모지 + ZWJ + 이모지 + ZWJ + 이모지
+            if (isZWJ(codePoint)) {
+                length--;
+                continue;
+            }
+
+            // ex: 👦🏼: 이모지 + 수정자
+            if (isEmojiModifier(codePoint)) {
+                continue;
+            }
+
             length++;
         }
 
         return length;
+    }
+
+    private static boolean isZWJ(int codePoint) {
+        return codePoint == ZWJ;
+    }
+
+    private static boolean isEmojiModifier(int codePoint) {
+        return 0x1F3FB <= codePoint && codePoint <= 0x1F3FF;
     }
 }
